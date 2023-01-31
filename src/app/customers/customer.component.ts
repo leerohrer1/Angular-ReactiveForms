1e71f2029b1ac03e751532f2a02e5f87ca8e9ae8
lee rohrer <lrohrer@hunter.com>
2023-01-31 16:01:24 -0600

    validation practice

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -3,6 +3,17 @@ import { FormGroup, FormBuilder, Validators, ValidatorFn, AbstractControl } from
 
 import { Customer } from './customer';
 
+function emailMathcer(c: AbstractControl): { [key: string]: boolean } | null {
+  const emailControl = c.get('email');
+  const confrimControl = c.get('confirmEmail');
+
+  if(emailControl.value === confrimControl.value) {
+    return null
+  }
+
+  return { 'match': true }
+}
+
 function ratingRange(min: number, max: number): ValidatorFn {
   return (c: AbstractControl): { [key: string]: boolean } | null => {
     if (c.value !== null && (isNaN(c.value) || c.value < min || c.value > max)) {
@@ -29,12 +40,19 @@ export class CustomerComponent implements OnInit {
     this.customerForm = this.fb.group({
       firstName: [ '', [ Validators.required, Validators.minLength(3) ] ],
       lastName: [ '', [ Validators.required, Validators.maxLength(50) ] ],
-      email: [ '', [ Validators.required, Validators.email ] ],
+      emailGroup: this.fb.group({
+        email: [ '', [ Validators.required, Validators.email ] ],
+        confirmEmail: [ '', Validators.required ]
+      }, { validator: emailMathcer }),
       phone: '',
       notification: 'email',
       rating: [null, ratingRange(1, 5)],
       sendCatalog: false,
     })
+
+    this.customerForm.get('notification').valueChanges.subscribe(
+      value => this.setNotification(value)
+    );
   }
 
   populateTestData(): void {
